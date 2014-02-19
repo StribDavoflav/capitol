623a328cbeda27cc33f3f15bb1d0dccef3ab1940
David Braunger <braundm@startribune.com>
2014-02-18 19:00:52 -0600

    Made temporary sprites for all of the chapters/sections.  Everything should be represented.

--- public/index.php
+++ public/index.php
@@ -88,7 +88,7 @@ function switchCapitolImage(newState) {
 
 <div id="tabs">
     <ul>
-      <li data-tab="0"><a href="#tabs-0">intro</a></li>
+      <li data-tab="0"><a href="#tabs-0">Welcome</a></li>
       <li data-tab="1"><a href="#tabs-1">Basement</a></li>
       <li data-tab="2"><a href="#tabs-2">Ground Floor</a></li>
       <li data-tab="3"><a href="#tabs-3">1st Floor</a></li>
@@ -101,7 +101,7 @@ function switchCapitolImage(newState) {
     <div id="tabs-0">
 		<div class="cardItems">
 			<div id="intro">
-				<a href="http://www.startribune.com/components/242933061.html" class="overlay cboxElement">
+				<a href="intro/index.html" class="overlay cboxElement">
 					<img src="http://stmedia.startribune.com/designimages/space.gif" width='304' height='168'>  
 				</a>
 			</div>
@@ -120,7 +120,7 @@ function switchCapitolImage(newState) {
 		<div class="cardItems">
 		 
 		    <h3>Starting at the bottom</h3>
-		    <p style="padding-bottom:20px">The floor has been under reconstruction since 2010 and is about 45% completed. An estimated $12 million has already been spent and another $10 million is budgeted for completion.</p>
+		    <p style="padding-bottom:20px">Long the home of Capitol staff and the Minnesota press, the basement has been gutted, its tenants relocated, and its tunnels rearranged to allow for mechanical work. By the end of the project, the basement area will be opened up to become a show space for visitors: adding an underground rotunda to serve as a public event space, restoring long-closed areas and exposing long painted-over stonework.  The basement is home to the public Rathskeller cafeteria, which has been restored to again show off German phrases along its walls, which will have additional seating added. Many are drinking themed, including "Ein frischer Trank, der Arbeit Dank" translated as "Enjoy a glass after a duty well performed."</p>
 		  
 
 			<div id="rathskellar">
@@ -154,16 +154,16 @@ function switchCapitolImage(newState) {
         <div class="cardItems">
                  
 			<h3>Ground Floor</h3>
-			<p style="padding-bottom:20px">The floor has been under reconstruction since 2010 and is about 45% completed. An estimated $12 million has already been spent and another $10 million is budgeted for completion.</p>
+			<p style="padding-bottom:20px">The ground floor, like much of the Capitol, will have its walls and ceilings cleaned and restored. The work may uncover original decorative paint in the north hall and elsewhere. Throughout the building, the restoration will add new directional signs because, as one report on the restoration noted, the current sparse signage means visiting the Capitol has become "prohibitively complex and inaccessible" for visitors and regulars alike. The East side of the floor will be opened, allowing for extended public space and a classroom.</p>
 
 
-			<div id="rathskellar">
+			<div id="g-staircases">
 				<a href="ground-floor/staircases.html" class="overlay cboxElement">
 					<img src="http://stmedia.startribune.com/designimages/space.gif" width='304' height='168'>  
 				</a>
 			</div>
 
-			<div id="supremeCourt">
+			<div id="g-halls">
 				<a href="ground-floor/halls-ceilings.html" class="overlay cboxElement">
 					<img src="http://stmedia.startribune.com/designimages/space.gif" width='304' height='168'>  
 				</a>
@@ -177,16 +177,16 @@ function switchCapitolImage(newState) {
          <div class="cardItems">
                  
 			<h3>First Floor</h3>
-			<p style="padding-bottom:20px">The floor has been under reconstruction since 2010 and is about 45% completed. An estimated $12 million has already been spent and another $10 million is budgeted for completion.</p>
+			<p style="padding-bottom:20px">This is the premier visitors' space at the Capitol, home of the rotunda where thousands of Minnesotans gather each year to make their voices heard. Both the governor and the attorney general's office on this now  on this floor will be moved during some parts of the restoration. This floor will become home to a new public information center to ease visitors trip to the Capitol. The Capitol's current elevators get new glass-front doors and a new service elevator will be added.</p>
 
 
-			<div id="rathskellar">
+			<div id="one-reception">
 				<a href="first-floor/receptionRoom.html" class="overlay cboxElement">
 					<img src="http://stmedia.startribune.com/designimages/space.gif" width='304' height='168'>  
 				</a>
 			</div>
 
-			<div id="supremeCourt">
+			<div id="one-interiors">
 				<a href="first-floor/interiors.html" class="overlay cboxElement">
 					<img src="http://stmedia.startribune.com/designimages/space.gif" width='304' height='168'>  
 				</a>
@@ -200,39 +200,39 @@ function switchCapitolImage(newState) {
 		<div class="cardItems">
                  
 			<h3>Second Floor</h3>
-			<p style="padding-bottom:20px">The floor has been under reconstruction since 2010 and is about 45% completed. An estimated $12 million has already been spent and another $10 million is budgeted for completion.</p>
+			<p style="padding-bottom:20px">The second floor of the Capitol houses the House and Senate chambers, where lawmakers meet in session to debate the issues of the day and vote on measures to deliver to the governor. It is also home to the state Supreme Court historic chambers, directly across the rotunda from the Senate, which could allow the Supreme Court chief justice and the Senate president to stare into each other's eyes. With construction, the second floor will get a thorough cleaning, decorative paint will be restored and an outdoor balcony will be repaired. The restoration will allow the addition of a lunch counter that will be open whenever lawmakers are meeting in session.</p>
 
-			<div id="rathskellar">
+			<div id="two-supreme">
 				<a href="second-floor/supremeCourt.html" class="overlay cboxElement">
 					<img src="http://stmedia.startribune.com/designimages/space.gif" width='304' height='168'>  
 				</a>
 			</div>
 
-			<div id="supremeCourt">
+			<div id="two-house">
 				<a href="second-floor/house.html" class="overlay cboxElement">
 					<img src="http://stmedia.startribune.com/designimages/space.gif" width='304' height='168'>  
 				</a>
 			</div>
 
-			<div id="hallways">
+			<div id="two-senate">
 				<a href="second-floor/senate.html" class="overlay cboxElement">
 					<img src="http://stmedia.startribune.com/designimages/space.gif" width='304' height='168'>  
 				</a>
 			</div>
 
-			<div id="pressOffices">
+			<div id="two-rotunda">
 				<a href="second-floor/rotunda.html" class="overlay cboxElement">
 					<img src="http://stmedia.startribune.com/designimages/space.gif" width='304' height='168'>  
 				</a>
 			</div>
 
-			<div id="hallways">
+			<div id="two-hallways">
 				<a href="second-floor/hallways.html" class="overlay cboxElement">
 					<img src="http://stmedia.startribune.com/designimages/space.gif" width='304' height='168'>  
 				</a>
 			</div>
 
-			<div id="terrace">
+			<div id="two-terrace">
 				<a href="second-floor/terrace.html" class="overlay cboxElement">
 					<img src="http://stmedia.startribune.com/designimages/space.gif" width='304' height='168'>  
 				</a>
@@ -249,22 +249,22 @@ function switchCapitolImage(newState) {
 		<div class="cardItems">
 		 
 		    <h3>Third floor</h3>
-		    <p style="padding-bottom:20px">The floor has been under reconstruction since 2010 and is about 45% completed. An estimated $12 million has already been spent and another $10 million is budgeted for completion.</p>
+		    <p style="padding-bottom:20px">The decorative paint on this floor, which provides access to the House and Senate galleries, has largely been refurbished to its Easter egg original pallet. From the third floor, visitors can look down to the rotunda below and up to the slightly damaged but still vibrant paint that rings the inside of the dome. The water-damaged dome is in the process of being repaired. Murals on this floor include one that shows a six-toed man, a small mouse and an animal skull. This floor, like others, will get new restrooms.</p>
 		  
 
-			<div id="rathskellar">
+			<div id="three-domeInterior">
 				<a href="third-floor/domeInterior.html" class="overlay cboxElement">
 					<img src="http://stmedia.startribune.com/designimages/space.gif" width='304' height='168'>  
 				</a>
 			</div>
 
-			<div id="supremeCourt">
+			<div id="three-artwork">
 				<a href="third-floor/artwork.html" class="overlay cboxElement">
 					<img src="http://stmedia.startribune.com/designimages/space.gif" width='304' height='168'>  
 				</a>
 			</div>
 
-			<div id="hallways">
+			<div id="three-gophers">
 				<a href="third-floor/gophers.html" class="overlay cboxElement">
 					<img src="http://stmedia.startribune.com/designimages/space.gif" width='304' height='168'>  
 				</a>
@@ -280,16 +280,16 @@ function switchCapitolImage(newState) {
 		<div class="cardItems">
                  
 			<h3>ON THE OUTSIDE</h3>
-			<p style="padding-bottom:20px">The floor has been under reconstruction since 2010 and is about 45% completed. An estimated $12 million has already been spent and another $10 million is budgeted for completion.</p>
+			<p style="padding-bottom:20px">Specialists are going through the painstaking process of cleaning and replacing the crumbling Georgia marble. Before renovation began in 2013, the deteriorating stone was sloughing off the building and causing safety problems and water damage. The Capitol grounds have also undergone renovation-related changes as temporary parking lots have been built into the mall to allow other work to be done. Eventually, the roof will be restored and repaired.</p>
 
 
-			<div id="rathskellar">
+			<div id="exteriorStone">
 				<a href="dome-exterior/exterior.html" class="overlay cboxElement">
 					<img src="http://stmedia.startribune.com/designimages/space.gif" width='304' height='168'>  
 				</a>
 			</div>
 
-			<div id="supremeCourt">
+			<div id="lionsHead">
 				<a href="dome-exterior/lion.html" class="overlay cboxElement">
 					<img src="http://stmedia.startribune.com/designimages/space.gif" width='304' height='168'>  
 				</a>
